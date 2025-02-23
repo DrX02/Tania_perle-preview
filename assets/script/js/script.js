459b269f7629245460ef4934bb194f21865b7b6c
DrX02 <leonelterry82@gmail.com>
2025-02-23 15:52:57 +0100

    mod link

--- assets/script/js/script.js
+++ assets/script/js/script.js
@@ -1,6 +1,17 @@
+
+// Mobile navbar
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 }); 
+
+
+// Carrousel
+const carousel = document.getElementById("carousel");
+
+carousel.addEventListener("wheel", (event) => {
+    event.preventDefault();
+    carousel.scrollLeft += event.deltaY; // Défilement horizontal avec la molette
+});
